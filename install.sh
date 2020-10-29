eb71db7215461511fb854e2240d41254e4faa895
Willi Carlsen <carlsenwilli@gmail.com>
2020-10-29 10:21:51 +0100

    Trying some new approach

--- install.sh
+++ install.sh
@@ -2,19 +2,53 @@
 
 set -euxo pipefail
 
+# Main setup
+setup() {
+    system_clock
+    create_mirrorlist
+    zap_disk
+    partition_disk
+    encrypt_main_partition
+    create_volumes
+    format_partitions
+    mount_file_system
+    install_base
+    chroot
+}
+
+# Chroot setup
+chrootsetup() {
+    timezone
+    localization
+    network_configuration
+    set_root_passwd
+    install_packages
+}
+
 # Update system clock
-timedatectl set-ntp true
+system_clock() {
+    echo 'Update system clock'
+    timedatectl set-ntp true
+}
 
 # Create mirrorlist
-pacman -Syy --noconfirm reflector
-reflector -c Denmark -a 6 --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Syyy
+create_mirrorlist() {
+    echo 'Creating mirrorlist'
+    pacman -Syy --noconfirm reflector
+    reflector -c Denmark -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+    pacman -Syyy
+}
 
 # Zap disk
-sgdisk --zap-all
+zap_disk() {
+    echo 'Zapping disk $DISK'
+    sgdisk --zap-all
+}
 
 # Parition disk
-gdisk $DISK << GDISK_CMDS
+partition_disk() {
+    echo 'Partion disk $DISK'
+    gdisk $DISK << GDISK_CMDS
 n
 1
 
@@ -29,70 +63,103 @@ w
 Y
 GDISK_CMDS
 
-UEFI_PARTITION=$(fdisk -l $DISK | grep 'EFI' | awk '{print $1}')
-MAIN_PARTITION=$(fdisk -l $DISK | grep 'LVM' | awk '{print $1}')
+    UEFI_PARTITION=$(fdisk -l $DISK | grep 'EFI' | awk '{print $1}')
+    MAIN_PARTITION=$(fdisk -l $DISK | grep 'LVM' | awk '{print $1}')
+}
+
 
 # Encrypt main partition
-echo $PASSWD | cryptsetup luksFormat $MAIN_PARTITION -d -
-echo $PASSWD | cryptsetup open $MAIN_PARTITION cryptlvm -d -
+encrypt_main_partition() {
+    echo 'Encrypt main partition'
+    echo $PASSWD | cryptsetup luksFormat $MAIN_PARTITION -d -
+    echo $PASSWD | cryptsetup open $MAIN_PARTITION cryptlvm -d -
+}
 
-pvcreate /dev/mapper/cryptlvm
-vgcreate vg1 /dev/mapper/cryptlvm
-lvcreate -L 40G vg1 -n root
-lvcreate -L 2G vg1 -n swap
-lvcreate -l 100%FREE vg1 -n home
+# Create physical and logical volumes
+create_volumes() {
+    echo 'Create volumes'
+    pvcreate /dev/mapper/cryptlvm
+    vgcreate vg1 /dev/mapper/cryptlvm
+    lvcreate -L 40G vg1 -n root
+    lvcreate -L 2G vg1 -n swap
+    lvcreate -l 100%FREE vg1 -n home
+}
 
 # Format partitions
-mkfs.fat -F32 $UEFI_PARTITION
-mkfs.ext4 /dev/vg1/root
-mkfs.ext4 /dev/vg1/home
-mkswap /dev/vg1/swap
+format_partitions() {
+    echo 'Format partitions'
+    mkfs.fat -F32 $UEFI_PARTITION
+    mkfs.ext4 /dev/vg1/root
+    mkfs.ext4 /dev/vg1/home
+    mkswap /dev/vg1/swap
+}
 
 # Mount file system
-mount /dev/vg1/root /mnt
-mkdir /mnt/home
-mount /dev/vg1/home /mnt/home
-mkdir /mnt/boot
-mount $UEFI_PARTITION /mnt/boot
-swapon /dev/vg1/swap
+mount_file_system() {
+    echo 'Mount file system'
+    mount /dev/vg1/root /mnt
+    mkdir /mnt/home
+    mount /dev/vg1/home /mnt/home
+    mkdir /mnt/boot
+    mount $UEFI_PARTITION /mnt/boot
+    swapon /dev/vg1/swap
+}
 
-# Pacstrap
-pacstrap /mnt base linux linux-firmware neovim intel-ucode lvm2
+# Install base
+install_base() {
+    echo 'Install base'
+    # Pacstrap
+    pacstrap /mnt base linux linux-firmware neovim intel-ucode lvm2
 
-# Generate filesystem table
-genfstab -U /mnt >> /mnt/etc/fstab
+    # Generate filesystem table
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
 
 # Configure system
-cp install.sh /mnt/root/install.sh
-chmod + /mnt/root/install.sh
-arch-chroot /mnt /root/install.sh setupchroot
+chroot() {
+    echo 'Configure system'
+    cp install.sh /mnt/root/install.sh
+    chmod + /mnt/root/install.sh
+    arch-chroot /mnt /root/install.sh setupchroot
+}
 
-# Unmount and reboot
-umount -a
-reboot
-
-setupchroot() {
-    # Timezone
+# Timezone
+timezone() {
+    echo 'Timezone'
     ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime
     hwclock --systohc
+}
 
-    # Localization
+# Localization
+localization() {
+    echo 'Localization'
     echo 'en_US.UTF-8 UTF-8' /etc/locale.gen # en_US.UTF-8 UTF-8
     locale-gen
     echo LANG=en_US.UTF-8 >> /etc/locale.conf
     echo KEYMAP=dk >> /etc/vconsole.conf
+}
 
-    # Network configuration
+
+# Network configuration
+network_configuration() {
+    echo 'Network configuration'
     echo $HOSTNAME >> /etc/hostname
     echo '127.0.0.1     localhost' > /etc/hosts
     echo '::1           localhost' > /etc/hosts
     echo '127.0.1.1     $HOSTNAME.localdomain   $HOSTNAME' > /etc/hosts
+}
 
-    # Set root password
+# Set root password
+set_root_passwd() {
+    echo 'Set root password'
     echo 'root:$PASSWD' | chpasswd
+}
 
-    # Install packages
+# Install packages
+install_packages() {
+    echo 'Install packages'
     pacman -Sy --noconfirm grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers git reflector bluez bluez-utils pulseaudio-bluetooth cups xdg-utils xdg-user-dirs
+}
 
     # Initramfs
     # sed -i "s/"
@@ -118,3 +185,10 @@ setupchroot() {
 
     # exit
 }
+
+if [[ $1 == setupchroot]]
+then
+    chrootsetup
+else
+    setup
+fi
