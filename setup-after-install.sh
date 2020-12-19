d975c1e73719487ea3139515e723b53b69372cc5
Alessandro Pirrotta <alessandro.pirrotta@gmail.com>
2020-12-19 16:19:17 +0100

    fix pacaur installation

--- setup-after-install.sh
+++ setup-after-install.sh
@@ -8,29 +8,24 @@ export VISUAL=vim
 export EDITOR=vim
 
 # Install pacaur
-install_pacaur() {
-    echo "Install pacaur"
-    if ! item="$(type -p "pacaur")" || [[ -z $item ]]; then
-        echo "pacaur is not installed"
-        sudo pacman -S --noconfirm --needed base-devel git
-        
-        mkdir -p ~/.software
-        cd ~/.software
-        sudo pacman -Syu --noconfirm --needed jq expac 
+if ! item="$(type -p "pacaur")" || [[ -z $item ]]; then
+    echo "pacaur is not installed"
+    sudo pacman -S --noconfirm --needed base-devel git
+    
+    mkdir -p ~/.software
+    cd ~/.software
+    sudo pacman -Syu --noconfirm --needed jq expac 
 
-        git clone https://aur.archlinux.org/auracle-git.git
-        cd auracle-git
-        makepkg -si
-        cd ..
+    git clone https://aur.archlinux.org/auracle-git.git
+    cd auracle-git
+    makepkg -si
+    cd ..
 
-        git clone https://aur.archlinux.org/pacaur.git
-        cd pacaur
-        makepkg -si
-        cd ..
-    else
-        echo "pacaur is already installed, doing nothing"
-    fi
-}
+    git clone https://aur.archlinux.org/pacaur.git
+    cd pacaur
+    makepkg -si
+    cd ../..
+fi
 
 sudo pacman -S --noconfirm --needed wget
 
@@ -90,4 +85,4 @@ GRUB_TIMEOUT="0"  # set to 0 to skip grub menu in case there is no dual boot
 sudo sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=${GRUB_TIMEOUT}/g" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-pacaur -S pug
+# pacaur -S pug
