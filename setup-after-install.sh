b41ffa6ae1dc265b77d93c7adca4fac388bc2406
Alessandro Pirrotta <alessandro.pirrotta@gmail.com>
2021-04-03 13:08:41 +0200

    Update setup-after-install.sh

--- setup-after-install.sh
+++ setup-after-install.sh
@@ -29,11 +29,11 @@ fi
 
 sudo pacman -S --noconfirm --needed wget
 
-wget -q -O pacman-list.pkg https://gist.githubusercontent.com/alessap/d006fbae581c07077a3fa8185802ff01/raw/d1f969a3d1860fbfe0a8a547e3630bd022574305/pacman-list.pkg
+wget -q -O pacman-list.pkg https://gist.githubusercontent.com/alessap/3120fb734b7257d6656da33820630612/raw/0dc9d1d84d56b595c0da572ee6a60c244213f237/pacman-list.pkg 
 sudo pacman -S --noconfirm --needed - < pacman-list.pkg
 rm pacman-list.pkg
 
-wget -q -O aur-list.pkg https://gist.githubusercontent.com/alessap/bb661806b5bd3f4554eb60df5308aa33/raw/719a3869a5e79d670d278f4945fd721fe60382e4/aur-list.pkg
+wget -q -O aur-list.pkg https://gist.githubusercontent.com/alessap/2e0a6863da0a9cc8195f5f50369a5852/raw/f0779a8dc2ac860aaa2dd513a9293846752cbfcd/aur-list.pkg
 sed '/pug/d' aur-list.pkg > aur-list-no-pug.pkg 
 xargs <aur-list-no-pug.pkg pacaur -S --noconfirm --needed --noedit 
 rm aur-list*pkg
